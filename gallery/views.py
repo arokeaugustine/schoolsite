bbcb60c1424c10eee83c9913b67e70ab4ae40a8f
Augustine Aroke <arokeaugustine@gmail.com>
2022-09-25 13:00:09 +0100

    modified pagination and lightbox view

--- gallery/views.py
+++ gallery/views.py
@@ -4,11 +4,10 @@ from django.core.paginator import Paginator
 
 def gallery(request):
     gallery = Gallery.objects.all().order_by('-upload_date')
-    paginator = Paginator(gallery, 3)
+    paginator = Paginator(gallery, 6)
     page_number = request.GET.get('page')
     gallery_obj = paginator.get_page(page_number)
-    # this next code is a hack for solving the pagination 
-    nums = 'a' * gallery_obj.paginator.num_pages
-    return render(request, 'gallery.html', {'gallery_obj': gallery_obj, 'nums':nums, })
+  
+    return render(request, 'gallery.html', {'gallery_obj': gallery_obj})
 
 
